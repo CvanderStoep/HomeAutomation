716cc7ea760ca57d7ba9d5dcd5f3e642269011c9
carlo <carlo.vanderstoep@gmail.com>
2020-10-10 16:08:56 +0200

    Adding Dataframes for logging

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from phue import Bridge
+import pandas as pd
 
+data = {'DateTime':[], 'T_gang':[], 'T_toilet':[], 'T_zolder':[]}
+df = pd.DataFrame(data)
 #documentation can be found at below link:
 # https://github.com/studioimaginaire/phue
 
-# b = Bridge('192.168.2.3') #connected to router
-b = Bridge('192.168.68.127') #connected to Deco mesh
-# b.connect() #this command is needed only once; press hue bridge button en run b.connect() command.
+# bridge = Bridge('192.168.2.3') #connected to router
+bridge = Bridge('192.168.68.127') #connected to Deco mesh
+# bridge.connect() #this command is needed only once; press hue bridge button en run bridge.connect() command.
 #returns a full dictionary
-huedictionary = b.get_api()
+huedictionary = bridge.get_api()
 print('Output full dictionary of Hue Bridge:')
 print('(Individual objects will be listed after this full dictionary)')
 for x,y in huedictionary.items():
@@ -16,7 +19,7 @@ for x,y in huedictionary.items():
 # print(len(huedictionary))
 
 #get a flat list of light objects
-lights = b.lights
+lights = bridge.lights
 # print(lights)
 print()
 print('Output for all lights:')
@@ -25,7 +28,7 @@ for light in lights:
         print(light.light_id, light.name, light.on, light.brightness, light.type)
 
 #get a flat list of sensor objects
-sensors = b.sensors
+sensors = bridge.sensors
 print()
 print('Output for all sensors:')
 
@@ -33,7 +36,7 @@ for sensor in sensors:
     print(sensor.sensor_id, sensor.name, sensor.state)
 
 #get a dictionary with sensor id as key
-sensors = b.get_sensor_objects('id')
+sensors = bridge.get_sensor_objects('id')
 temp_sensor_gang1 = sensors[75].state['temperature']/100 #temp in degC
 temp_sensor_toilet = sensors[17].state['temperature']/100
 temp_sensor_zolder = sensors[8].state['temperature']/100
@@ -44,18 +47,28 @@ print('Temperature Toilet BG: ', temp_sensor_toilet)
 print('Temperature Zolder 2e etage: ', temp_sensor_zolder)
 
 #get a dictionary with lights id as the key
-lights = b.get_light_objects('id')
+lights = bridge.get_light_objects('id')
 # print(len(lights))
 # for i in range(1,len(lights)):
 #     print(lights[i])
 
-print(b.get_light(11, 'on'))
+print(bridge.get_light(11, 'on'))
 if temp_sensor_zolder > 24:
-    b.set_light([11], 'on', True)
-    b.set_light([11], 'bri', 50)
+    bridge.set_light([11], 'on', True)
+    bridge.set_light([11], 'bri', 50)
 if temp_sensor_zolder > 25:
-    b.set_light([12], 'on', True)
-    b.set_light([12], 'bri', 100)
+    bridge.set_light([12], 'on', True)
+    bridge.set_light([12], 'bri', 100)
+
+current_status = bridge.get_light(11, 'on')
+while True:
+    if current_status != bridge.get_light(11, 'on'):
+        print ('change in status')
+        current_status = bridge.get_light(11, 'on')
+        if current_status:
+            print('lights turned on')
+        else:
+            print('lights turned off')
 
 
 
