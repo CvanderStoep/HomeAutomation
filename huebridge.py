af03899b156934af44e33913126a101b4873ec4e
carlo <carlo.vanderstoep@gmail.com>
2020-10-22 17:35:15 +0200

    english naming convention for sensors

--- huebridge.py
+++ huebridge.py
@@ -55,18 +55,18 @@ def getsensors(sensortype):
 
     # get a dictionary with sensor id as key
     sensors = bridge.get_sensor_objects('id')
-    temp_sensor_zolder = sensors[8].state['temperature'] / 100
-    temp_sensor_toilet = sensors[17].state['temperature'] / 100
-    temp_sensor_gang = sensors[75].state['temperature'] / 100  # temp in degC
+    temp_sensor_second_floor = sensors[8].state['temperature'] / 100
+    temp_sensor_ground_floor = sensors[17].state['temperature'] / 100
+    temp_sensor_first_floor = sensors[75].state['temperature'] / 100  # temp in degC
 
     print()
-    print('Temperature Sensor[8] - Zolder 2e etage: ', temp_sensor_zolder)
-    print('Temperature Sensor[17] - Toilet BG: ', temp_sensor_toilet)
-    print('Temperature Sensor[75] - Gang 1e etage: ', temp_sensor_gang)
+    print('Temperature Sensor[8] - second_floor: ', temp_sensor_second_floor)
+    print('Temperature Sensor[17] - ground_floor: ', temp_sensor_ground_floor)
+    print('Temperature Sensor[75] - first_floor: ', temp_sensor_first_floor)
     return
 
 
-def getDelftweather():
+def getoutsideweather():
     from private_info import complete_url #combines api key and city for the temperature
     response = requests.get(complete_url)
     x = response.json()
@@ -75,103 +75,100 @@ def getDelftweather():
     return current_temperature
 
 
-#TODO add proper index to dataframe
-#TODO convert dictionay to dataframe
-#TODO rename T-gang etc naar T_first floor etc
-#TODO organize files (example; move bridge operations to separate file;
-# move example to use bridge module to another demo file)
-
 def update(frame):
     # activate the sensors and get the data
     sensors = bridge.get_sensor_objects('id')
-    temp_sensor_gang = sensors[75].state['temperature'] / 100  # temp in degC
-    temp_sensor_toilet = sensors[17].state['temperature'] / 100
-    temp_sensor_zolder = sensors[8].state['temperature'] / 100
-    temp_Delft = getDelftweather()  # get the current Temperature in Delft using OpenWeatherData
+    temp_sensor_first_floor = sensors[75].state['temperature'] / 100  # temp in degC
+    temp_sensor_ground_floor = sensors[17].state['temperature'] / 100
+    temp_sensor_second_floor = sensors[8].state['temperature'] / 100
+    temp_outside = getoutsideweather()  # get the current Temperature in outside using OpenWeatherData
 
     # add new time and sensor temperature data to a .csv file
     new_data = {'DateTime': [datetime.now().strftime('%Y-%m-%d %H:%M:%S')],
-                'T_gang': [temp_sensor_gang],
-                'T_toilet': [temp_sensor_toilet],
-                'T_zolder': [temp_sensor_zolder],
-                'T_Delft': [temp_Delft]}
+                'T_first_floor': [temp_sensor_first_floor],
+                'T_ground_floor': [temp_sensor_ground_floor],
+                'T_second_floor': [temp_sensor_second_floor],
+                'T_outside': [temp_outside]}
     newdf = pd.DataFrame(new_data)  # only the latest data
     newdf.to_csv(DATA_FILE, mode='a', header=False)
     print(new_data)
-    # print('T-zolder last update: ', sensors[8].state['lastupdated'])
+    # print('T-second_floor last update: ', sensors[8].state['lastupdated'])
 
     time_data.append(datetime.now())
-    T_gang_data.append(temp_sensor_gang)
-    T_zolder_data.append(temp_sensor_zolder)
-    T_toilet_data.append(temp_sensor_toilet)
-    T_Delft_data.append(temp_Delft)
+    T_first_floor_data.append(temp_sensor_first_floor)
+    T_second_floor_data.append(temp_sensor_second_floor)
+    T_ground_floor_data.append(temp_sensor_ground_floor)
+    T_outside_data.append(temp_outside)
 
-    line_gang.set_data(time_data, T_gang_data)
-    line_zolder.set_data(time_data, T_zolder_data)
-    line_toilet.set_data(time_data, T_toilet_data)
-    line_delft.set_data(time_data, T_Delft_data)
+    line_first_floor.set_data(time_data, T_first_floor_data)
+    line_second_floor.set_data(time_data, T_second_floor_data)
+    line_ground_floor.set_data(time_data, T_ground_floor_data)
+    line_outside.set_data(time_data, T_outside_data)
 
-    pyplot.plot(time_data, T_gang_data, color='blue')
-    pyplot.plot(time_data, T_zolder_data, color='black')
-    pyplot.plot(time_data, T_toilet_data, color='red')
-    pyplot.plot(time_data, T_Delft_data, color='orange')
+    pyplot.plot(time_data, T_first_floor_data, color='blue')
+    pyplot.plot(time_data, T_second_floor_data, color='black')
+    pyplot.plot(time_data, T_ground_floor_data, color='red')
+    pyplot.plot(time_data, T_outside_data, color='orange')
     figure.gca().relim()
     figure.gca().autoscale_view()
 
-    pyplot.legend(['first floor {T: .1f} deg C'.format(T = temp_sensor_gang),
-                   'second floor {T: .1f} deg C'.format(T=temp_sensor_zolder),
-                   'ground floor {T: .1f} deg C'.format(T=temp_sensor_toilet),
-                   'outside(Delft) {T: .1f} deg C'.format(T=temp_Delft)])
+    pyplot.legend(['first floor {T: .1f} deg C'.format(T = temp_sensor_first_floor),
+                   'second floor {T: .1f} deg C'.format(T=temp_sensor_second_floor),
+                   'ground floor {T: .1f} deg C'.format(T=temp_sensor_ground_floor),
+                   'outside {T: .1f} deg C'.format(T=temp_outside)])
 
     ax.set_xlabel('Date-Time')
     ax.set_ylabel('Temp (deg C)')
 
 
-    return [line_gang, line_zolder, line_toilet, line_delft]
+    return [line_first_floor, line_second_floor, line_ground_floor, line_outside]
 
 
 # below the main program loop starts
 
 
 def read_data():
-    data = {'DateTime': [], 'T_gang': [], 'T_toilet': [], 'T_zolder': [], 'T_Delft': []}
+    data = {'DateTime': [], 'T_first_floor': [], 'T_ground_floor': [], 'T_second_floor': [], 'T_outside': []}
     df = pd.DataFrame(data)
     hue_data_file = Path(DATA_FILE)
     if not hue_data_file.is_file():  # if the file does not exist; create the file and initialize the data
         df.to_csv(DATA_FILE)
-        time_data, T_gang_data, T_zolder_data, T_toilet_data, T_Delft_data = [], [], [], [], []
+        time_data, T_first_floor_data, T_second_floor_data, T_ground_floor_data, T_outside_data = [], [], [], [], []
     else:  # read historic data from file
         old_df = pd.read_csv(DATA_FILE)
         time_data = pd.to_datetime(old_df['DateTime'], format='%Y-%m-%d %H:%M:%S').to_list()
-        T_gang_data = old_df['T_gang'].to_list()
-        T_zolder_data = old_df['T_zolder'].to_list()
-        T_toilet_data = old_df['T_toilet'].to_list()
-        T_Delft_data = old_df['T_Delft'].to_list()
+        T_first_floor_data = old_df['T_first_floor'].to_list()
+        T_second_floor_data = old_df['T_second_floor'].to_list()
+        T_ground_floor_data = old_df['T_ground_floor'].to_list()
+        T_outside_data = old_df['T_outside'].to_list()
 
-    return time_data, T_gang_data, T_zolder_data, T_toilet_data, T_Delft_data
+    return time_data, T_first_floor_data, T_second_floor_data, T_ground_floor_data, T_outside_data
 
 
 def initialise_figure():
     figure, ax = pyplot.subplots()
-    line_gang, = pyplot.plot_date(time_data, T_gang_data, '-', color='blue')
-    line_zolder, = pyplot.plot_date(time_data, T_zolder_data, '-', color='black')
-    line_toilet, = pyplot.plot_date(time_data, T_toilet_data, '-', color='red')
-    line_delft, = pyplot.plot_date(time_data, T_Delft_data, '-', color='orange')
-    return figure, ax, line_gang, line_zolder, line_toilet, line_delft
+    line_first_floor, = pyplot.plot_date(time_data, T_first_floor_data, '-', color='blue')
+    line_second_floor, = pyplot.plot_date(time_data, T_second_floor_data, '-', color='black')
+    line_ground_floor, = pyplot.plot_date(time_data, T_ground_floor_data, '-', color='red')
+    line_outside, = pyplot.plot_date(time_data, T_outside_data, '-', color='orange')
+    return figure, ax, line_first_floor, line_second_floor, line_ground_floor, line_outside
 
 
 if __name__ == '__main__':
-    (time_data, T_gang_data, T_zolder_data, T_toilet_data, T_Delft_data) = read_data()
+    #read old data file
+    (time_data, T_first_floor_data, T_second_floor_data, T_ground_floor_data, T_outside_data) = read_data()
 
     # TODO prevent loss of connection with bridge
+    #setup connection with the hue bridge
     initbridge()
     # getdictionary()
-    # getsensors("temperature")
+    getsensors("temperature")
     # getlights()
 
-    (figure, ax, line_gang, line_zolder, line_toilet, line_delft) = initialise_figure()
+    #inititalize figure for animation
+    (figure, ax, line_first_floor, line_second_floor, line_ground_floor, line_outside) = initialise_figure()
 
     # start the animation  with an interval in ms
-    animation = FuncAnimation(figure, update, interval=1000)
+    animation = FuncAnimation(figure, update, interval=60000)
 
     pyplot.show()
